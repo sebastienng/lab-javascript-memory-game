984857b53d8469193cf4dee30ec357e517c73fce
Sebastien Niang <sebastien.niang@gmail.com>
2022-08-04 16:21:50 +0200

    some bugs if i click multiple times on one card

--- src/index.js
+++ src/index.js
@@ -30,6 +30,7 @@ const memoryGame = new MemoryGame(cards);
 window.addEventListener('load', (event) => {
   let html = '';
   memoryGame.cards.forEach((pic) => {
+
     html += `
       <div class="card" data-card-name="${pic.name}">
         <div class="back" name="${pic.img}"></div>
@@ -45,7 +46,40 @@ window.addEventListener('load', (event) => {
   document.querySelectorAll('.card').forEach((card) => {
     card.addEventListener('click', () => {
       // TODO: write some code here
-      console.log(`Card clicked: ${card}`);
+      if(memoryGame.pickedCards.length<2) card.classList.toggle('turned');
+
+      
+     
+      memoryGame.pickedCards.push(card);
+      console.log(memoryGame.pickedCards);
+     
+     setTimeout(()=>{
+  
+
+      if(memoryGame.pickedCards.length>=2){
+        
+        const cardObject1 = cards.find(item => item.name === memoryGame.pickedCards[0].getAttribute('data-card-name'));
+        const cardObject2 = cards.find(item => item.name === memoryGame.pickedCards[1].getAttribute('data-card-name'));
+        console.log(cardObject1);
+        if(!memoryGame.checkIfPair(cardObject1.name,cardObject2.name)){
+  
+       
+          memoryGame.pickedCards[0].classList.toggle('turned');
+          memoryGame.pickedCards[1].classList.toggle('turned');
+          memoryGame.pickedCards=[];
+        
+        }else{
+          memoryGame.pickedCards=[];
+
+        }
+        
+
+      }
+      document.querySelector('#pairs-clicked').innerHTML=memoryGame.pairsClicked;
+      document.querySelector('#pairs-guessed').innerHTML=memoryGame.pairsGuessed;
+     },3000)
+    
+
     });
   });
 });
--- src/memory.js
+++ src/memory.js
@@ -2,17 +2,30 @@ class MemoryGame {
   constructor(cards) {
     this.cards = cards;
     // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
 
-  shuffleCards() {
+  shuffleCards(arrayCards) {
     // ... write your code here
+    if(!arrayCards)return undefined;
+    return arrayCards.sort( ()=> Math.random()-0.5);
   }
 
   checkIfPair(card1, card2) {
     // ... write your code here
+    this.pairsClicked++;
+    if(card1===card2){
+      this.pairsGuessed++;
+      return true;
+    }
+    return false;
   }
 
   checkIfFinished() {
     // ... write your code here
+
+    return this.pairsGuessed===this.cards.length/2
   }
 }
